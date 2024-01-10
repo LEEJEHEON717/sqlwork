85de6b48af4ee539192c88dacbc47ee585e7889b
LEEJEHEON717 <leejehun0717@naver.com>
2024-01-10 21:55:17 +0900

    member_table

--- mysqlworks/springdb/boards_users_table.sql
+++ mysqlworks/springdb/boards_users_table.sql
@@ -1,56 +1,51 @@
+create database springdb;
 use springdb;
 
 drop table if exists users;
-create table users (
-	id        	 	bigint primary key auto_increment,
-    userid    		varchar(20) unique,
-    userpasswd  	varchar(20) not null,
-    username   		varchar(30) not null,
-    userage     	int
+create table users(
+	id bigint primary key auto_increment,
+    userid varchar(20) unique,
+    userpasswd varchar(20) not null,
+    username varchar(30) not null,
+    userage int
 );
 
--- user를 참조하는 boards 테이블 생성
-drop table if exists boards;
-create table boards (
-    id            	bigint primary key auto_increment,
-    userid      	varchar(20) not null,	
-    boardtitle      varchar(100) not null,
-    boardcontent  	 text not null,
+-- users를 참조하는 boards 테이블 생성
+drop table if exists boards; 
+create table boards(
+	id  bigint primary key auto_increment,
+    boardtitle varchar(100) not null,
+    userid varchar(20) not null,
+    boardcontent text not null,
     hit int default 0,
-    createtime datetime default now(),
-    updatetime datetime,
+    createdtime datetime default now(),
+    updatedtime datetime,
     foreign key(userid) references users(userid) on delete cascade
 );
 
--- board를 참조하는 reply 테이블 생성
+-- boards를 참조하는 reply 테이블 생성
 drop table if exists reply;
 create table reply(
-	id				bigint primary key auto_increment,
-	boardid			bigint,
-	replyer			varchar(20) not null,
-	replyContent	varchar(4000) not null,
-	createdTime		datetime default now(),
-	updatedTime		datetime,
-    foreign key(boardid) references boards(id) on delete cascade
+	id  bigint primary key auto_increment,
+    boardid bigint,
+    replyer varchar(20) not null,
+    replycontent varchar(4000) not null,
+    createdtime datetime default now(),
+    updatedtime datetime,
+    foreign key(boardid) references boards(id) 
+    on delete cascade
 );
--- board 테이블 구조 보기
+-- boards 테이블 구조 보기
 desc boards;
 
 select * from users;
 select * from boards order by id desc;
 select * from reply;
-select * from reply where boardid=3;
 
-
-select * from reply where id = 7;
+select * from reply where id=1;
 
 -- 6번 댓글 삭제
-delete from reply where id = 6;
-
--- 13번 댓글 수정
-update reply
-set replycontent = '공지사항 수정함'
-where id = 13;
+delete from reply where id = 7;
 
 -- 재귀 복사
 insert into boards (boardtitle, userid, boardcontent)
@@ -58,10 +53,12 @@ insert into boards (boardtitle, userid, boardcontent)
 
 -- 전체 게시글 수
 select count(id) from boards;
--- limit 시작 인덱스, 게시글 개수
+
+-- limit 시작인덱스, 게시글 개수
 select * from boards order by id asc limit 0, 5; -- 1,2,3,4,5 -> 1page
 select * from boards order by id asc limit 1, 5; -- 2,3,4,5,6 -> 2page
 select * from boards order by id asc limit 2, 5; -- 3,4,5,6,7 -> 3page
 
 -- 1page(0, 5), 2page(5, 5), 3page(10, 5)
 select * from boards order by id desc limit 5, 5;
+
--- mysqlworks/springdb/todo_table.sql
+++ mysqlworks/springdb/todo_table.sql
@@ -3,26 +3,10 @@ use springdb;
 
 drop table if exists tbl_todo;
 create table tbl_todo(
-	tno 			bigint primary key auto_increment,
-	title 			varchar(100) not null,
-	writer			varchar(20) not null,
-	createddate 	datetime default now()
+	tno  bigint primary key auto_increment,
+    title  varchar(100) not null,
+    writer varchar(20) not null,
+    createddate datetime default now()
 );
 
-select * from tbl_todo order by tno desc;
-select * from tbl_todo order by tno asc limit 3; 	-- 제일 앞에서 부터
-select * from tbl_todo order by tno desc limit 3;	-- 제일 뒤에서 부터
-select * from tbl_todo order by tno asc limit 2, 4; -- 2다음 부터 4개 표시 ex)3,4,5,6
-
--- 검색
-select * from tbl_todo 
-where title like '%운동%' or writer like '%대상현%'
-order by tno desc;
-
--- 재귀 복사
-insert into tbl_todo(title, writer)
-(select title, writer from tbl_todo);
-
-select count(tno) from tbl_todo;
-
-truncate table tbl_todo;
+select * from tbl_todo;
