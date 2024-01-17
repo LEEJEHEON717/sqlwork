7c5520b9f5213bcc735061ba8d1ddf939688612d
LEEJEHEON717 <leejehun0717@naver.com>
2024-01-17 12:23:08 +0900

    bootdb

--- mysqlworks/springdb/bootdb.sql
+++ mysqlworks/springdb/bootdb.sql
@@ -4,4 +4,10 @@ create database bootdb;
 use bootdb;
 
 select * from tbl_member;
+select * from tbl_board order by id desc;
+
+select * from board;
+select * from board order by id asc;
+select * from board order by id desc;
+
 desc tbl_member;
